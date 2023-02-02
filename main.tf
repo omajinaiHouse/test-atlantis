a4e399580cc963c0bfc76e8a40e2f3edc201d13d
pomcho555 <ono.fda03@gmail.com>
2023-02-03 01:47:06 +0900

    add ec2

--- main.tf
+++ main.tf
@@ -5,18 +5,39 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "s3" {
+    bucket = "test-terraform-2023-02-03"
+    key    = "atlantis/test.tf"
+    region = "ap-northeast-1"
+  }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-northeast-1"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name = "HelloWorld"
   }
 }
