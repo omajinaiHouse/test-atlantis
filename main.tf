da14f9b161af5974cc2be62800e3276736d6c173
pomcho555 <ono.fda03@gmail.com>
2023-03-31 14:49:03 +0900

    wrong commit

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t10.micro"
+  # instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
